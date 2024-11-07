3ce753a9194e2e6fedb7649fb558de1d61645057
Koen Severijns <KD.Severijns@student.han.nl>
2024-11-07 15:04:58 +0100

    WIP

--- eindopdracht/arduino_server/abnf.c
+++ eindopdracht/arduino_server/abnf.c
@@ -54,47 +54,111 @@ bool http_message(struct stream stream) {
 }
 
 // remaining ABFN functions are stubs
+// ABFN funcs are responssible for reading their own tokens that are linked with their function
+// Funcs only return true if what they need to check is true, otherwise they always return false
 
 static bool field_line(struct stream stream) {
+  // field-name ":" *SP field-value *SP
+  if(field_name(stream)){
+    //check for ::
+    struct token next = peekToken(stream);
+    if(next.value[0] = ":"){
+      //read SP
+      while(readTokenType(stream, SP)){}
+      if(field_value(stream)){
+        //read SP
+        while(readTokenType(stream, SP)){}
+        return true;
+      }
+    }
+  }
   return false;
 }
 
 static bool field_name(struct stream stream) {
-  return false;
+  //http_token
+  return http_token(stream);
 }
 
 static bool field_value(struct stream stream) {
+  // VCHAR *( SP / HTAB / VCHAR )
+  //Char followed by any amount of SP tabs and chars stop when next is a enter
+  struct token next = peekToken(stream);
+  int i = 0; //this will save field value one day
+  if(next.value == VCHAR){
+    while(next.type == VCHAR || next.type == SP || next.type == HTAB){
+      i++;
+      readToken(stream);
+      next = peekToken(stream);
+    }
+  }
+  if(i != 0){
+    return true;
+  }
   return false;
 }
 
-static bool http_token(struct stream stream) {
+static bool http_token(struct stream stream) { //probs gonna need some pointer magic to get the values up to original caller
+  //any amount of tchar
+  struct token next = peekToken(stream);
+  int i = 0; //this will save the token one day
+  while(isTchar(stream)){
+    i++;
+    readToken(stream);
+    next = peekToken(stream);
+  }
+  if(i != 0){
+    return true;
+  }
   return false;
 }
 
 static bool http_version(struct stream stream) {
+  //"HTTP" "/" DIGIT "." DIGIT
   return false;
 }
 
 static bool message_body(struct stream stream) {
+  //*octet
   return false;
 }
 
-static bool method(struct stream stream) { return false; }
+static bool method(struct stream stream) {
+  //HTTP token
+  return http_token(stream);
+}
 
 static bool request_line(struct stream stream) {
+  //Request line =  method SP request-target SP http-version
+  if(method(stream)){
+    //Check for space
+    if(readTokenType(stream, SP)){
+      if(request_target(stream)){
+        //check for space
+        if(readTokenType(stream, SP)){
+          //Last check returns true if last check passes
+          return http_version(stream);
+        } 
+      } 
+    } 
+  }
   return false;
 }
 
 static bool request_target(struct stream stream) {
+  //Origin form
   return false;
 }
 
 static bool origin_form(struct stream stream) {
+  //origin-form = "/" [ http-token [ origin-form ] ]
+
   return false;
 }
 
 static bool start_line(struct stream stream) {
-  return false;
+  // Start line == requestline. If requestline == true return true else return false
+  return request_line(stream);
 }
 
 static bool tchar(struct stream stream, char result[],
@@ -135,6 +199,39 @@ static bool tchar(struct stream stream, char result[],
     }
   }
 
+  return false;
+}
+
+static bool isTchar(struct stream stream) {
+  struct token next = peekToken(stream);
+
+  if (next.type == DIGIT) {
+    return true;
+  }
+
+  if (next.type == ALPHA) {
+    return true;
+  }
+
+  if (next.type == VCHAR) {
+    switch (next.value[0]) {
+    case '!': // falls through on purpose
+    case '#':
+    case '$':
+    case '%':
+    case '&':
+    case '\'':
+    case '*':
+    case '+':
+    case '-':
+    case '.':
+    case '^':
+    case '`':
+    case '|':
+    case '~':
+      return true;
+    }
+  }
   return false;
 }
 
